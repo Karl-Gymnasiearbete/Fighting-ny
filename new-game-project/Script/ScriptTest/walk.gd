e00ab261e21758d8453a2937b88499d50f95d488
Karl-Gymnasiearbete <karl.borrstade02@edu.vellinge.se>
2025-10-02 17:57:16 +0200

    fixade movement

--- new-game-project/Script/ScriptTest/walk.gd
+++ new-game-project/Script/ScriptTest/walk.gd
@@ -5,15 +5,17 @@ var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var speed := 400
 
 func Physics_Update(delta: float) -> void:
-	var direction := Input.get_vector("left", "right", "", "") # no jump here!
+	var direction := Input.get_vector("left", "right", "", "")
 
 	if direction == Vector2.ZERO:
+		# Stop moving when no input
+		Steve.velocity.x = 0
 		Transitioned.emit(self, "Idle")
 	else:
-		# Keep vertical velocity (Y) for gravity, overwrite only X
+		# Update horizontal movement
 		Steve.velocity.x = direction.x * speed
 
-	# Apply gravity
+	# Gravity (always applied)
 	if not Steve.is_on_floor():
 		Steve.velocity.y += gravity * delta
 	else:
