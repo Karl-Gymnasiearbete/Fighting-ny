9e59fb825471f5b40244c10c441c133f58343e37
sahxli <sixtenrosengren@gmail.com>
2025-11-03 16:26:02 +0100

    spawn position

--- new-game-project/Scener/Arena/main.gd
+++ new-game-project/Scener/Arena/main.gd
@@ -6,10 +6,10 @@ func _ready():
 func spawn_player():
 	var p1_scene = load("res://Character/" + Global.p1_character + ".tscn")
 	var player1 = p1_scene.instantiate()
-	player1.position = Vector2(200,300)
+	player1.position = Vector2(450,300)
 	add_child(player1)
 	
 	var p2_scene = load("res://Character/" + Global.p2_character + ".tscn")
 	var player2 = p2_scene.instantiate()
-	player2.position = Vector2(600,300)
+	player2.position = Vector2(750,300)
 	add_child(player2)
