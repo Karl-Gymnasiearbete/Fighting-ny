2307d7947d173abf6f64fd2a72bc2638b3013976
sahxli <sixtenrosengren@gmail.com>
2025-11-03 14:53:53 +0100

    character select funkar

--- new-game-project/Scener/Arena/main.gd
+++ new-game-project/Scener/Arena/main.gd
@@ -6,12 +6,10 @@ func _ready():
 func spawn_player():
 	var p1_scene = load("res://Character/" + Global.p1_character + ".tscn")
 	var player1 = p1_scene.instantiate()
-	player1.postion = Vector2(200,300)
-	player1.player_number = 1
+	player1.position = Vector2(200,300)
 	add_child(player1)
 	
-	var p2_scene = load("res://Character/" + Global.p2_charact + ".tscn")
+	var p2_scene = load("res://Character/" + Global.p2_character + ".tscn")
 	var player2 = p2_scene.instantiate()
-	player2.postion = Vector2(600,300)
-	player2.player_number = 2
-	add_child(player1)
+	player2.position = Vector2(600,300)
+	add_child(player2)
